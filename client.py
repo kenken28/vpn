66e022dac8cd26b6080f061ff944073dacbc9fb6
kenken28 <xux@live.ca>
2015-10-19 18:36:34 -0700

    tried 2048bit prime

--- client.py
+++ client.py
@@ -2,36 +2,15 @@
 
 import socket               # Import socket module
 import sys
-import random as r
+import mylib as m
 
-PRIME = 277666483056069257952962430614829339539
-P_ROOT = 2
-
-def mSend(s, m):
-    if type(m) is not str:
-        m = str(m)
-    if len(m)%1024 == 0:
-        m += '@end'
-    s.sendall(m)
-
-def mRecv(s):
-    whole = ''
-    while 1:
-        chunk = s.recv(1024)
-        if len(chunk)<1024:
-            if chunk != '@end':
-                whole += chunk
-            break
-        whole += chunk
-    return whole
     
 #HOST = raw_input("Enter Server IP address: ")
 HOST = socket.gethostname()
-PORT = 50021
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.connect((HOST, PORT))
+    s.connect((HOST, m.PORT))
 except socket.error as msg:
     s.close()
     s = None
@@ -39,20 +18,22 @@ except socket.error as msg:
     sys.exit(1)
 
 myMsg = 'You are SERVER'
-print repr(mRecv(s))                 # Test receiveing
-mSend(s, myMsg)                      # Test sending
+print repr(m.mRecv(s))                 # Test receiveing
+m.mSend(s, myMsg)                      # Test sending
 
-b = r.randint(2**256,2**257)
-B = pow(P_ROOT, b, PRIME)
+b = m.randI(2**2049,2**2050-1)
+B = pow(m.P_ROOT, b, m.PRIME128)
 
-A = int(mRecv(s))
-mSend(s, B)
+A = int(m.mRecv(s))
+m.mSend(s, B)
 
-Cs = pow(A, b, PRIME)
+Cs = pow(A, b, m.PRIME)
 print Cs
-Ss = int(mRecv(s))
+Ss = int(m.mRecv(s))                # varify key, should not exist in real code
 if Ss == Cs:
-    print "yeah!"
+    print "key varified!"
+
+
 
 s.close()
 
--- server.py
+++ server.py
@@ -2,34 +2,12 @@
 
 import socket
 import sys
-import random as r
-
-PRIME = 277666483056069257952962430614829339539
-P_ROOT = 2
-
-def mSend(s, m):
-    if type(m) is not str:
-        m = str(m)
-    if len(m)%1024 == 0:
-        m += '@end'
-    s.sendall(m)
-
-def mRecv(s):
-    whole = ''
-    while 1:
-        chunk = s.recv(1024)
-        if len(chunk)<1024:
-            if chunk != '@end':
-                whole += chunk
-            break
-        whole += chunk
-    return whole
+import mylib as m
 
 HOST = socket.gethostname()
-PORT = 50021
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
-    s.bind((HOST, PORT))
+    s.bind((HOST, m.PORT))
     s.listen(1)
 except socket.error as msg:
     s.close()
@@ -39,17 +17,20 @@ conn, addr = s.accept()
 print 'Connected by', addr
 
 myMsg = 'You are CLIENT'
-mSend(conn, myMsg)                      # Test sending 
-print repr(mRecv(conn))                 # Test receiveing
+m.mSend(conn, myMsg)                      # Test sending 
+print repr(m.mRecv(conn))                 # Test receiveing
 
-a = r.randint(2**256,2**257)
-A = pow(P_ROOT, a, PRIME)
+a = m.randI(2**2049,2**2050-1)
+A = pow(m.P_ROOT, a, m.PRIME128)
 
-mSend(conn, A)
-B = int(mRecv(conn))
+m.mSend(conn, A)
+B = int(m.mRecv(conn))
 
-Ss = pow(B, a, PRIME)
+Ss = pow(B, a, m.PRIME)
 print Ss
-mSend(conn, Ss)
+m.mSend(conn, Ss)                       # varify key, should not exist in real code
+
+
+
 
 conn.close()
